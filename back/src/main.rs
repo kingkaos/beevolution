687b648323350f68448c1343718aeac14c1571e5
kingkaos <da.cebulla@gmail.com>
2024-04-04 22:47:37 +0200

    chore: remove unused function

--- back/src/main.rs
+++ back/src/main.rs
@@ -6,12 +6,6 @@ use simple_logger::SimpleLogger;
 
 mod app;
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 fn main() {
     SimpleLogger::new()
         .with_level(LevelFilter::Debug)
