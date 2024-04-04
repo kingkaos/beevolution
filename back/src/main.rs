20831f1aa1198b4f7f06c87a8d8090308ba58266
kingkaos <da.cebulla@gmail.com>
2024-04-04 22:46:47 +0200

    feat: add logger

--- back/src/main.rs
+++ back/src/main.rs
@@ -1,6 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use log::{info, LevelFilter};
+use simple_logger::SimpleLogger;
+
 mod app;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -10,6 +13,13 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    SimpleLogger::new()
+        .with_level(LevelFilter::Debug)
+        .env()
+        .init()
+        .unwrap();
+    info!("Starting beevolution");
+
     let app = app::init_app();
     app.run(|_, _| {})
 }
