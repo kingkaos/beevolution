95e7942b19ef42543c0225b8ff32aece7e2f5810
kingkaos <da.cebulla@gmail.com>
2024-04-06 23:01:05 +0200

    refactor: separate file/folder check from init store

--- back/src/store.rs
+++ back/src/store.rs
@@ -1,33 +1,44 @@
 use log::error;
-use std::env;
-use std::fs;
-use std::io;
 use std::path::Path;
+use std::{fs, io};
 
-fn touch(path: &Path) -> io::Result<()> {
-    match fs::OpenOptions::new().create(true).write(true).open(path) {
+fn try_create_folder(path: &Path) -> io::Result<()> {
+    match fs::metadata(path) {
         Ok(_) => Ok(()),
-        Err(e) => Err(e),
+        Err(_) => match fs::create_dir_all(path) {
+            Ok(_) => Ok(()),
+            Err(e) => Err(e),
+        },
+    }
+}
+
+fn try_create_file(path: &Path) -> io::Result<()> {
+    match fs::metadata(path) {
+        Ok(_) => Ok(()),
+        Err(_) => match fs::OpenOptions::new().create(true).write(true).open(path) {
+            Ok(_) => Ok(()),
+            Err(e) => Err(e),
+        },
     }
 }
 
 pub fn init_store_file() -> bool {
     // default location of the store is ~/.config/beevolution/config.json
     // The path and file will be created if they do not exist.
-    let home = env::var("HOME").unwrap();
-    let path = format!("{}/.config/beevolution", home);
-    // create folders if they do not exist
-    if fs::metadata(&path).is_err() {
-        fs::create_dir_all(&path).unwrap_or_else(|why| {
-            error!("Unable to create path {:?}", why.kind());
-        });
-    }
-    let path_to_store = format!("{}/config.json", path);
-    if fs::metadata(&path_to_store).is_err() {
-        touch(&Path::new(&path_to_store)).unwrap_or_else(|why| {
-            error!("Unable to create file {:?}", why.kind());
-        });
-    };
+    let path_to_store = format!(
+        "{}/.config/{}/config.json",
+        env!("HOME"),
+        env!("CARGO_PKG_NAME")
+    );
+    let path_to_store = Path::new(path_to_store.as_str());
+    // create folders if they don't exist
+    try_create_folder(&(path_to_store.parent().unwrap()))
+        .unwrap_or_else(|why| error!("Unable to create folder: {:?}", why));
+
+    // try create store file if it doesn't exist
+    try_create_file(&path_to_store)
+        .unwrap_or_else(|why| error!("Unable to create file: {:?}", why));
+
     // returns true if file exists else false
     Path::new(&path_to_store).exists()
 }
