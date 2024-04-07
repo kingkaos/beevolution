eb0cd90baeae7472a64221121c2c24e947513bfa
kingkaos <da.cebulla@gmail.com>
2024-04-07 19:42:26 +0200

    feat: add tauri menu setup

--- back/src/app.rs
+++ back/src/app.rs
@@ -1,7 +1,34 @@
-use tauri::{generate_context, generate_handler, App, Builder, Manager};
+use tauri::menu::{Menu, MenuItem, PredefinedMenuItem, Submenu};
+use tauri::{generate_context, generate_handler, App, AppHandle, Builder, Manager, Runtime, Wry};
+
+fn init_menu<R: Runtime>(handle: &AppHandle) -> tauri::Result<Menu<Wry>> {
+    Menu::with_items(
+        handle,
+        &[
+            &Submenu::with_items(
+                handle,
+                env!("CARGO_PKG_NAME"),
+                true,
+                &[
+                    &PredefinedMenuItem::close_window(handle, None)?,
+                    #[cfg(target_os = "macos")]
+                    &MenuItem::new(handle, "Hello", true, None::<&str>)?,
+                    &PredefinedMenuItem::quit(handle, None)?,
+                ],
+            )?,
+            &Submenu::with_items(
+                handle,
+                "View",
+                true,
+                &[&PredefinedMenuItem::fullscreen(handle, None)?],
+            )?,
+        ],
+    )
+}
 
 pub fn init_app() -> App {
     Builder::default()
+        .menu(|handle| init_menu::<Wry>(handle))
         .plugin(tauri_plugin_shell::init())
         .setup(|app| {
             #[cfg(debug_assertions)] // only include this code on debug builds
