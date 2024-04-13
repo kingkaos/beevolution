ecfc7cbadee2ccdf1520c4d7a6ee5ff86b3cb524
kingkaos <da.cebulla@gmail.com>
2024-04-13 23:29:00 +0200

    refactor: rename Component for identification
    SidebarNav is now SettingsNav.

--- front/pages/Settings.vue
+++ front/pages/Settings.vue
@@ -1,20 +1,22 @@
 <template>
-  <div class="space-y-0.5">
-    <h2 class="text-2x1 font-bold tracking-tight">
-      Settings
-    </h2>
-    <p class="text-muted-foreground">
-      Change the bahaviour of beevolution
-    </p>
-  </div>
-  <Separator class="my-6" />
-  <div class="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-    <aside class="-mx-4 lg:w-1/5">
-      <SidebarNav />
-    </aside>
-    <div class="flex-1 lg:max-w-2xl">
-      <div class="space-y-6">
-        <slot />
+  <div class="p-5">
+    <div class="space-y-0.5">
+      <h2 class="text-2x1 font-bold tracking-tight">
+        Settings
+      </h2>
+      <p class="text-muted-foreground">
+        Change the behaviour of beevolution
+      </p>
+    </div>
+    <Separator class="my-6" />
+    <div class="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+      <aside class="-mx-4 lg:w-1/5">
+        <SettingsNav />
+      </aside>
+      <div class="flex-1 lg:max-w-2xl">
+        <div class="space-y-6">
+          <slot />
+        </div>
       </div>
     </div>
   </div>
@@ -22,5 +24,5 @@
 
 <script setup lang="ts">
 import { Separator } from '@/components/ui/separator'
-import SidebarNav from '../components/SidebarNav.vue'
+import SettingsNav from '@/components/SettingsNav.vue'
 </script>
