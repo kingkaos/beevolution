833cbf73ab0383599f1d2e1e6a063fdf9ee6aac6
kingkaos <da.cebulla@gmail.com>
2024-04-14 00:03:10 +0200

    feat: add form layout for database settings

--- front/views/SettingsDatabase.vue
+++ front/views/SettingsDatabase.vue
@@ -1,9 +1,33 @@
 <template>
   <Settings>
-    Database
+    <div>
+      <h3 class="text-lg font-medium">
+	Database
+      </h3>
+      <p class="text-sm text-muted-foreground">
+	Update your database settings.
+      </p>
+    </div>
+    <Separator />
+    <Form v-slot="{ setValues }" class="space-y-8 p-2" @submit="onSubmit">
+    <FormField v-slot="{ componentField }" name="name">
+      <FormItem>
+        <FormLabel>Host</FormLabel>
+        <FormControl>
+          <Input type="text" placeholder="e.g. 127.0.0.1" v-bind="componentField" />
+        </FormControl>
+        <FormDescription>
+	  This is the host address of the database.
+        </FormDescription>
+        <FormMessage />
+      </FormItem>
+    </FormField>
+    </Form>
   </Settings>
 </template>
 
 <script setup lang="ts">
-import Settings from '../pages/Settings.vue'
+  import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+  import { Separator } from '@/components/ui/separator';
+  import Settings from '../pages/Settings.vue';
 </script>
