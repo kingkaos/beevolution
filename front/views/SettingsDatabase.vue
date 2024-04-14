e07cb8b4682738087836e82a405d6ca76ef513b9
kingkaos <da.cebulla@gmail.com>
2024-04-14 02:18:44 +0200

    feat: add form to handle database settings

--- front/views/SettingsDatabase.vue
+++ front/views/SettingsDatabase.vue
@@ -9,25 +9,43 @@
       </p>
     </div>
     <Separator />
-    <Form v-slot="{ setValues }" class="space-y-8 p-2" @submit="onSubmit">
-    <FormField v-slot="{ componentField }" name="name">
-      <FormItem>
-        <FormLabel>Host</FormLabel>
-        <FormControl>
-          <Input type="text" placeholder="e.g. 127.0.0.1" v-bind="componentField" />
-        </FormControl>
-        <FormDescription>
-	  This is the host address of the database.
-        </FormDescription>
-        <FormMessage />
-      </FormItem>
-    </FormField>
+    <Form v-slot="{ setValues }" class="space-y-8" @submit="onSubmit">
+      <FormField v-slot="{ componentField }" name="dbHost">
+        <FormItem>
+          <FormLabel class="pr-5">
+	    Host
+	  </FormLabel>
+          <FormControl>
+            <Input type="text" placeholder="e.g. 127.0.0.1" v-bind="componentField" />
+          </FormControl>
+          <FormDescription>
+  	    This is the host address of the database.
+          </FormDescription>
+          <FormMessage />
+        </FormItem>
+      </FormField>
+
+      <FormField v-slot="{ dbPort }" class="space-y-8 p-2" name="dbPort">
+	<FormItem>
+	  <FormLabel class="pr-5">
+	    Port
+	  </FormLabel>
+	  <FormControl>
+	    <Input type="text" placeholder="e.g. 5432" v-bind="dbPort" />
+	  </FormControl>
+	  <FormDescription>
+	    This is the port of the database.
+	  </FormDescription>
+	</FormItem>
+      </FormField>
     </Form>
   </Settings>
 </template>
 
 <script setup lang="ts">
-  import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
-  import { Separator } from '@/components/ui/separator';
-  import Settings from '../pages/Settings.vue';
+import { Input } from '@/components/ui/input';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel } from '@/components/ui/form'
+import { Separator } from '@/components/ui/separator';
+
+import { Settings } from '@/pages';
 </script>
